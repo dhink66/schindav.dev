b5f61edf9c2e132fa1fa0856d02b2015d8bc743c
dhink66 <davidhinkdesign@gmail.com>
2024-11-18 00:10:43 +0100

    Update V3 nové linky a pic

--- index.js
+++ index.js
@@ -17,41 +17,40 @@ updateDate();
 
 function changeLang(langCode) {
   if (langCode === "cz") {
-    document.querySelector("h1").innerText =
-      "Dobrý den, jmenuji se David Schinkmann";
+    document.querySelector("h1").innerText = "Ahoj👋🏽, já jsem David";
     document.querySelector(".welcome").innerText =
-      "vítejte v mém světě softwaru.";
+      "vítej v mém světě softwaru.";
     document.querySelector(".journey").innerText =
-      "Něco málo o mé cestě. Začal jsem jako grafik v realitách a pak jsem našel kód. Od té doby navrhuji různé webové stránky, aplikace a užitečné pomocníky. Například:";
-    document.querySelector(".link").innerText = "inkOT studio";
-    document.querySelector(".game").innerText = "prvních 100 hra";
+      "Něco málo o mé cestě. Začal jsem jako grafik v realitách a pak jsem našel kód. Od té doby navrhuji různé weby, aplikace a užitečné pomocníky. Například:";
+    document.querySelector(".link").innerText = "PujcMoto web";
+    document.querySelector(".game").innerText = "100 hra";
     document.querySelector(".hobby").innerText =
-      "Velkou část mého času tvoří také sport, hlavně cvičení v posilovně a na hrazdách, jízda na horském kole nebo turistika. Krom toho rád čtu nebo cestuji s přáteli.";
+      "Velkou část mého času tvoří také sport, převážně cvičení s vlastní vahou, jízda na kole nebo turistika. Krom toho rád čtu nebo cestuji s přáteli.";
     document.querySelector(
       ".insides"
-    ).innerHTML = `Pokud se chcete podívat na detaily některých projektů nebo jen koncepty, zde je můj
-      <a class="link" href="https://github.com/davidhink">Github</a> nebo
-      <a class="link" href="#">Dribbble</a>. Také mě můžete kontaktovat mailem :
+    ).innerHTML = `Více mé tvorby je k dispozici na
+      <a class="link" href="https://github.com/dhink66" target="_blank">Githubu (kód)</a> nebo
+      <a class="link" href="https://dribbble.com/davhink" target="_blank">Dribbblu (grafika)</a>. Pro jakékoliv info mail :
       <a class="link" href="mailto:david.schinkmann@gmail.com"
         >david.schinkmann@gmail.com</a
       >`;
 
     // cz část
   } else if (langCode === "en") {
-    document.querySelector("h1").innerText = "Hi, i am David Schinkmann";
+    document.querySelector("h1").innerText = "Hi 👋🏽, i am David";
     document.querySelector(".welcome").innerText =
       "welcome in my world of software engineering.";
     document.querySelector(".journey").innerText =
-      "Little bit about my journey, started as a graphic designer and than i found code. Since than i been designing different apps webs or any kinda helpers. For example:";
-    document.querySelector(".link").innerText = "inkOT Website";
-    document.querySelector(".game").innerText = "first 100 Game";
+      "Little bit about my journey. Started as a graphic designer in real estate and than i found code. Since than i have been designing different applications, websites or any kinda useful helpers. For example:";
+    document.querySelector(".link").innerText = "PujcMoto website";
+    document.querySelector(".game").innerText = "100 Game";
     document.querySelector(".hobby").innerText =
       " Big part of my time are also sports, mainly caliesthetics, mountain biking or just hiking. Other than that i enjoy reading or traveling with friends.";
     document.querySelector(
       ".insides"
-    ).innerHTML = `If you want to look at the insides, mainly on
-    <a class="link" href="https://github.com/davidhink">Github</a> or
-    <a class="link" href="#">Dribbble</a>, or you can reach me via email on
+    ).innerHTML = `If you want to look at more details, mainly on
+    <a class="link" href="https://github.com/dhink66" target="_blank">Github (code)</a> or
+    <a class="link" href="https://dribbble.com/davhink" target="_blank">Dribbble (graphics)</a>, or you can reach me via email on
     :
     <a class="link" href="mailto:david.schinkmann@gmail.com"
       >david.schinkmann@gmail.com</a
